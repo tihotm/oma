6cfd97a62d4dc64afb127526dab75b7d28086742
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 19:04:14 -0300

    test: battery v4 13 terminal block precedence

---
+++ tests/test_battery20_causal_v4_13.py
@@ -0,0 +1,12 @@
+from oma.terminal import TerminalDecision,canonical_terminal_policy,evaluate_terminal_barrier
+from oma.validation import ValidationDecision,ValidationObservation
+
+def test_terminal_block_dominates_stale_and_not_done():
+ p=canonical_terminal_policy('term'); ids=sorted(p.required_node_ids); obs=[]
+ for i in ids:
+  d=ValidationDecision.ACCEPT
+  if i==ids[0]: d=ValidationDecision.BLOCK
+  elif i==ids[1]: d=ValidationDecision.STALE
+  elif i==ids[2]: d=ValidationDecision.NOT_DONE
+  obs.append(ValidationObservation(i,d,'r'))
+ assert evaluate_terminal_barrier(p,tuple(obs),requested_action='COMMIT').decision is TerminalDecision.BLOCK
