4a86400bf80e91da4ca74105d5fee89f7d448c62
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 12:14:17 -0300

    feat: export acceptance core

---
+++ src/oma/__init__.py
@@ -0,0 +1,15 @@
+from .acceptance import (
+    AcceptanceContext,
+    AcceptanceDecision,
+    AcceptanceResult,
+    Evidence,
+    evaluate_acceptance,
+)
+
+__all__ = [
+    "AcceptanceContext",
+    "AcceptanceDecision",
+    "AcceptanceResult",
+    "Evidence",
+    "evaluate_acceptance",
+]
