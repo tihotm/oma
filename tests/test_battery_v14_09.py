46bae9f5bb7dedf00ef416d10b642e6556acc53d
tihotm <igorbrito.n1@hotmail.com>
2026-08-20 09:48:22 -0300

    test: audit empty string terminal container

---
+++ tests/test_battery_v14_09.py
@@ -0,0 +1,10 @@
+import pytest
+from oma.commit import AcceptanceSnapshot, CommitState, CommitToken, commit_if_current
+
+
+def test_empty_string_terminal_container_crashes_transition():
+    s=AcceptanceSnapshot('snap','sub','state','pb','obl','ev','ledger',1,1,'root')
+    t=CommitToken('tok','snap','sub',1,True)
+    c=CommitState('sub','state','pb','obl','ev','ledger',1,1,frozenset(),'','root')
+    with pytest.raises(TypeError):
+        commit_if_current(s,t,c,terminal_commit_id='term')
